be93db1aba38223c719892c2866555fa216c4cb0
Mike Kerslake <theonlymikeever@gmail.com>
2017-09-22 15:13:29 -0400

    inital build of store

--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -1,4 +1,7 @@
 import { combineReducers } from 'redux'
+import axios from 'axios'
+import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
 
 //INTIIAL STATE
 const initialState = {
@@ -7,19 +10,32 @@ const initialState = {
 }
 
 //ACTION TYPES
-const GET_STUDENTS = 'GET_STUDENTS';
-const GET_CAMPUSES = 'GET_CAMPUSES';
-const ADD_STUDENT = 'ADD_STUDENT';
-const ADD_CAMPUS = 'ADD_CAMPUS';
-const DELETE_STUDENT = 'DELETE_STUDENT';
-const DELETE_CAMPUS = 'DELETE_CAMPUS';
-const UPDATE_CAMPUS = 'UPDATE_CAMPUS';
+const GOT_STUDENTS = 'GOT_STUDENTS';
+const GOT_CAMPUSES = 'GOT_CAMPUSES';
 
 //ACITON CREATORS
+export function getStudents (students){
+  const action = { type: GOT_STUDENTS, students }
+  return action;
+}
+export function getCampuses (campuses){
+  const action = { type: GOT_CAMPUSES, campuses }
+  return action;
+}
 
 //REDUCER
 const rootReducer = function(state = initialState, action) {
   switch(action.type) {
+    case GOT_STUDENTS:
+      return {
+        ...state,
+        students: action.students
+    }
+    case GOT_CAMPUSES:
+      return {
+        ...state,
+        campuses: action.campuses
+    }
     default: return state
   }
 };
