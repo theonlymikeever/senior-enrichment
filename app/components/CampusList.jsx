9e6b3509beaada3ff3942fc27091b75a4fffb357
Mike Kerslake <theonlymikeever@gmail.com>
2017-09-25 12:56:25 -0400

    CSS tweaks

--- app/components/CampusList.jsx
+++ app/components/CampusList.jsx
@@ -1,29 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import CampusForm from './CampusForm';
 
 function CampusList(props){
     const { campuses } = props;
     return (
-    <div className="col-sm-12 col-md-8">
+    <div className="col-sm-12 col-md-12">
       <h2>Campuses</h2>
       <div className="row">
-        {
-          campuses.map( campus => {
-            return (
-              <div className="col-sm-6" key={ campus.id }>
-                <div className="card">
-                 <img className="card-img-top img-responsive" src={campus.imageUrl} alt="Card image cap" />
-                  <div className="card-body">
-                    <h4 className="card-title">{ campus.bame }</h4>
-                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <Link className="btn btn-primary" to={`/campuses/${campus.id}`}>{ campus.name }</Link>
+      <div className="col-sm-8">
+        <div className="row">
+          {
+            campuses.map(( campus ) => {
+              return (
+                <div className="col-sm-6" key={ campus.id }>
+                  <div className="card">
+                   <img className="card-img-top img-responsive" src={campus.imageUrl} alt="Card image cap" />
+                    <div className="card-body">
+                      <h4 className="card-title">{ campus.bame }</h4>
+                      <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                      <Link className="btn btn-primary" to={`/campuses/${campus.id}`}>{ campus.name }</Link>
+                    </div>
                   </div>
                 </div>
-              </div>
-            )
-          })
-        }
+              )
+            })
+          }
+        </div>
+        </div>
+        <div className="col-sm-4 pull-right">
+          <CampusForm />
+        </div>
       </div>
     </div>
     )
